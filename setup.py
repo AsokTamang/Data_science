3514004e13ab4fcb937bcc2c3e413ab91e06f4fe
AsokTamang <ashoktmg2058@gmail.com>
2026-03-16 11:48:35 -0400

    setup file

--- setup.py
+++ setup.py
@@ -1,11 +1,24 @@
 from setuptools import find_packages,setup
 
+from typing import List
+
+
+hyphen = '-e .'
+def get_requirements(filepath:str)->List[str]:
+    with open(filepath) as f:
+        requirements = f.read().splitlines()
+        if hyphen in requirements:  #as we have used -e . in our requirements.txt file to connect with our setup.py file, so we must remove them here
+            requirements.remove(hyphen)
+        return requirements  #returning the list of the required packages
+
+
 
 setup(
     name='mlproject',
     version='0.0.1',
     packages=find_packages(),
-    install_requires=['pandas','numpy','seaborn','matplotlib'],
+    install_requires=get_requirements('requirements.txt'),
     author='asok',
     author_email='ashoktmg205@gmail.com',
 )
+
